5986128fbdbb91914a5da36d48aca1039f8da849
Ian-Pruett <ianpruett14@gmail.com>
2020-04-26 18:50:34 -0700

    Initial Commit

---
+++ CovidNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CovidNotes
+//
+//  Created by Ian Pruett on 4/26/20.
+//  Copyright © 2020 Ian Pruett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
